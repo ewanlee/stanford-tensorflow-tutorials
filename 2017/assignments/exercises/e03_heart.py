20b6402c79b4378d7ba1a3b121355aa204aadac5
ewan lee <whl@aida.aida>
2018-01-16 15:03:20 +0800

    Assignment 1 heart

--- 2017/assignments/exercises/e03_heart.py
+++ 2017/assignments/exercises/e03_heart.py
@@ -1,41 +1,48 @@
-""" Starter code for logistic regression model to solve OCR task
-with MNIST in TensorFlow
-MNIST dataset: yann.lecun.com/exdb/mnist/
-Author: Chip Huyen
-Prepared for the class CS 20SI: "TensorFlow for Deep Learning Research"
-cs20si.stanford.edu
-"""
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
 import time
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import shutil
+
+tf.set_random_seed(42)
+np.random.seed(42)
 
 # Define paramaters for the model
 learning_rate = 0.01
 batch_size = 128
-n_epochs = 25
+n_epochs = 500
 
 # Step 1: Read in data
-# using TF Learn's built in function to load MNIST data to the folder data/mnist
-mnist = input_data.read_data_sets('./data/mnist', one_hot=True)
+data = pd.read_csv('./data/heart/heart.csv').as_matrix()
+famhist = data[:, 4]
+data_without_famhist = np.delete(data, 4, 1)
+
+lb = preprocessing.LabelBinarizer()
+famhist_bin = lb.fit_transform(famhist)
+ohenc = preprocessing.OneHotEncoder()
+famhist_onehot = ohenc.fit_transform(famhist_bin).toarray()
+
+data = np.c_[data_without_famhist, famhist_onehot]
+features = data[:, :-1]
+labels = ohenc.fit_transform(data[:, -1].reshape(-1, 1)).toarray()
+X_train, X_test, Y_train, Y_test = train_test_split(features, labels, test_size=0.2)
 
 # Step 2: create placeholders for features and labels
-# each image in the MNIST data is of shape 28*28 = 784
-# therefore, each image is represented with a 1x784 tensor
-# there are 10 classes for each image, corresponding to digits 0 - 9.
-# Features are of the type float, and labels are of the type int
-X = tf.placeholder(tf.float32, shape=[batch_size, 784], name='image')
-Y = tf.placeholder(tf.float32, shape=[batch_size, 10], name='label')
+X = tf.placeholder(tf.float32, shape=[None, 10], name='feature')
+Y = tf.placeholder(tf.float32, shape=[None, 2], name='label')
 
 # Step 3: create weights and bias
 # weights and biases are initialized to 0
 # shape of w depends on the dimension of X and Y so that Y = X * w + b
 # shape of b depends on Y
-w = tf.Variable(tf.random_normal(shape=[784, 10], stddev=0.01), name='weights', dtype=tf.float32)
-b = tf.Variable(tf.zeros([1, 10]), name='biases', dtype=tf.float32)
+w = tf.Variable(tf.random_normal(shape=[10, 2], stddev=0.01), name='weights', dtype=tf.float32)
+b = tf.Variable(tf.zeros([1, 2]), name='biases', dtype=tf.float32)
 
 # Step 4: build model
 # the model that returns the logits.
@@ -53,50 +60,48 @@ entropy = tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=logits, name=
 loss = tf.reduce_mean(entropy, name='loss')
 tf.summary.scalar('mean_loss', loss)
 
+preds = tf.nn.softmax(logits)
+correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32))# need numpy.count_nonzero(boolarr) :(
+
 # Step 6: define training op
 # using gradient descent to minimize loss
-optimize = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
+optimize = tf.train.AdamOptimizer().minimize(loss)
 
 merged = tf.summary.merge_all()
 
 with tf.Session() as sess:
-    train_writer = tf.summary.FileWriter('./graphs/03/logistic_reg/train', sess.graph)
-    test_writer = tf.summary.FileWriter('./graphs/03/logistic_reg/test', sess.graph)
+    shutil.rmtree('./graphs/e03/heart/')
+    train_writer = tf.summary.FileWriter('./graphs/e03/heart/train', sess.graph)
+    test_writer = tf.summary.FileWriter('./graphs/e03/heart/test', sess.graph)
 
     start_time = time.time()
     sess.run(tf.global_variables_initializer())
-    n_batches = int(mnist.train.num_examples/batch_size)
     train_steps = 0
     for i in range(n_epochs): # train the model n_epochs times
-        total_loss = 0
-
-        for _ in range(n_batches):
+        for j in range(0, len(X_train), batch_size):
             train_steps += 1
-            X_batch, Y_batch = mnist.train.next_batch(batch_size)
+            X_batch, Y_batch = X_train[j : j + batch_size], Y_train[j : j + batch_size]
             # TO-DO: run optimizer + fetch loss_batch
-            _, loss_batch, train_summary = sess.run([optimize, loss, merged], feed_dict={X: X_batch, Y: Y_batch})
+            _, loss_batch, acc_batch, train_summary = sess.run([optimize, loss, accuracy, merged], feed_dict={X: X_batch, Y: Y_batch})
             train_writer.add_summary(train_summary, train_steps)
-            total_loss += loss_batch
-        print('Average loss epoch {0}: {1}'.format(i, total_loss/n_batches))
+            print('Average loss/acc batch {}: {} / {:.2f}%'.format(
+                train_steps, loss_batch, acc_batch / len(X_batch) * 100))
 
     print('Total time: {0} seconds'.format(time.time() - start_time))
 
     print('Optimization Finished!') # should be around 0.35 after 25 epochs
 
     # test the model
-    preds = tf.nn.softmax(logits)
-    correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(Y, 1))
-    accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32)) # need numpy.count_nonzero(boolarr) :(
 
-    n_batches = int(mnist.test.num_examples/batch_size)
     test_steps = 0
     total_correct_preds = 0
 
-    for i in range(n_batches):
+    for i in range(0, len(X_test), batch_size):
         test_steps += 1
-        X_batch, Y_batch = mnist.test.next_batch(batch_size)
+        X_batch, Y_batch = X_test[i : i + batch_size], Y_test[i : i + batch_size]
         accuracy_batch, test_summary = sess.run([accuracy, merged], feed_dict={X: X_batch, Y:Y_batch})
         test_writer.add_summary(test_summary, test_steps)
         total_correct_preds += accuracy_batch
 
-    print('Accuracy {0}'.format(total_correct_preds/mnist.test.num_examples))
+    print('Accuracy {:.2f}%'.format(total_correct_preds / len(X_test) * 100))
